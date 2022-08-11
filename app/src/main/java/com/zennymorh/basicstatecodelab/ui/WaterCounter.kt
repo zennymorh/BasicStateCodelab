20688bad8fb267d3c9157683b997557d23bfb537
Zainab Jimoh <jimohzainab4753@gmail.com>
2022-08-11 01:21:56 +0100

    remember in composition

--- app/src/main/java/com/zennymorh/basicstatecodelab/ui/WaterCounter.kt
+++ app/src/main/java/com/zennymorh/basicstatecodelab/ui/WaterCounter.kt
@@ -1,4 +1,37 @@
 package com.zennymorh.basicstatecodelab.ui
 
-class WaterCounter {
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        var count by rememberSaveable { mutableStateOf(0) }
+        if (count > 0) {
+            var showTask by remember { mutableStateOf(true) }
+            if (showTask) {
+                WellnessTaskItem(
+                    taskName = "Have you taken a walk today?",
+                    onClose = { showTask = false })
+            }
+            Text("You've had $count glasses.")
+        }
+
+        Row(Modifier.padding(top = 8.dp)) {
+            Button(onClick = { count++ }, enabled = count < 10) {
+                Text("Add one")
+            }
+            Button(onClick = {count = 0},  Modifier.padding(start = 8.dp)) {
+                Text("Clear water count")
+            }
+        }
+        
+    }
 }
